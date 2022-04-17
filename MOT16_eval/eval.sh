adde7578276d62c94a23db9ed428643c1eb01bb0
ZHS <com.zhs@gmail.com>
2022-04-17 23:34:20 +0800

    update eval.sh

--- MOT16_eval/eval.sh
+++ MOT16_eval/eval.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
+#BSUB -J track
+#BSUB -o %J.out
+#BSUB -e %J.err
+#BSUB -q gpu_v100
+#BSUB -gpu "num=1:mode=exclusive_process:aff=yes"
+
+module load anaconda3
+source activate
+conda deactivate
+conda activate torch1.10
+
 
 set +e
 
 
 # start from clean slate
-for i in data.zip MOT16.zip
-do
-	zip -T $i
-	if [ $? -eq 0 ]
-	then
-		echo 'zip is ok'
-		
-	else
-		echo 'zip corrupted, deleting'
-		rm -rf $i
-	fi
-done
+#for i in data.zip MOT16.zip
+#do
+#	zip -T $i
+#	if [ $? -eq 0 ]
+#	then
+#		echo 'zip is ok'
+#
+#	else
+#		echo 'zip corrupted, deleting'
+#		rm -rf $i
+#	fi
+#done
 
 
 # create output folder if it doesn't exist
@@ -28,17 +39,19 @@ fi
 
 
 # clone evaluation repo if it does not exist
-if [ ! -d ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval ]
+if [ ! -d MOT16_eval/TrackEval ]
 then
 	echo 'Cloning official MOT16 evaluation repo'
-	git clone https://github.com/JonathonLuiten/TrackEval ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval
+	git clone https://github.com/JonathonLuiten/TrackEval TrackEval
 	# download quick start data folder if it does not exist
-	if [ ! -d ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data ]
+	if [ ! -d ~/datasets/MOT/data ]
 	then
 		# download data
+		echo 'Downloading TrackEval data...'
 		wget -nc https://omnomnom.vision.rwth-aachen.de/data/TrackEval/data.zip -O ~/Yolov5_DeepSort_Pytorch/data.zip
 		# unzip
 		unzip -q ~/Yolov5_DeepSort_Pytorch/data.zip -d ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/
+		echo 'Done.'
 		# delete zip
 		#rm data.zip
 	fi
@@ -46,42 +59,45 @@ fi
 
 
 # create MOT16 folder if it doesn't exist
-if [ ! -d ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/MOT16 ]
+if [ ! -d MOT16_eval/TrackEval/data/MOT16 ]
 then
-	mkdir -p ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/MOT16
+	mkdir -p MOT16_eval/TrackEval/data/MOT16
 fi
 
 
 # if MOT16 data not unziped, then download, unzip and lastly remove zip MOT16 data
-if [[ ! -d ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/MOT16/train ]] && [[ ! -d ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/MOT16/test ]]
+if [[ ! -d ~/datasets/MOT/MOT16/train ]] && [[ ! -d ~/datasets/MOT/MOT16/test ]]
 then
 	# download data
+	echo 'Downloading MOT16 data...'
 	wget -nc https://motchallenge.net/data/MOT16.zip -O ~/Yolov5_DeepSort_Pytorch/MOT16.zip
 	# unzip
     unzip -q MOT16.zip -d ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/MOT16/
+    echo 'Done.'
 	# delete zip
 	#rm MOT16.zip
 fi
 
 
 # create folder to place tracking results for this method
-mkdir -p ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/trackers/mot_challenge/MOT16-train/ch_yolov5m_deep_sort/data/
+mkdir -p MOT16_eval/TrackEval/data/trackers/mot_challenge/MOT16-train/ch_yolov5m_deep_sort/data/
 
 # inference on 4 MOT16 video sequences at the same time
 # suits a 4GB GRAM GPU, feel free to increase if you have more memory
 N=1
 
 # generate tracking results for each sequence
-for i in MOT16-02 MOT16-04 MOT16-05 MOT16-09 MOT16-10 MOT16-11 MOT16-13 
+echo 'Generating tracking results for each sequence...'
+for i in MOT16-02 MOT16-04 MOT16-05 MOT16-09 MOT16-10 MOT16-11 MOT16-13
 do
 	(
 		# change name to inference source so that each thread write to its own .txt file
-		if [ ! -d ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/MOT16/train/$i/$i ]
+		if [ ! -d ~/datasets/MOT/MOT16/train/$i/$i ]
 		then
-			mv ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/MOT16/train/$i/img1/ ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/MOT16/train/$i/$i
+			mv ~/datasets/MOT/MOT16/train/$i/img1/ ~/datasets/MOT/MOT16/train/$i/$i
 		fi
 		# run inference on sequence frames
-		python3 track.py --source ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/MOT16/train/$i/$i --save-txt --evaluate --yolo_model yolov5/weights/crowdhuman_yolov5m.pt --classes 0 --exist-ok
+		python3 track.py --source ~/datasets/MOT/MOT16/train/$i/$i --save-txt --evaluate --yolo_model ~/.cache/torch/checkpoints/crowdhuman_yolov5m.pt --classes 0 --exist-ok --device $CUDA_VISIBLE_DEVICES
 	    # move generated results to evaluation repo
 	) &
 	# https://unix.stackexchange.com/questions/103920/parallelize-a-bash-for-loop
@@ -90,7 +106,7 @@ do
 	then
         # now there are $N jobs already running, so wait here for any job
         # to be finished so there is a place to start next one.
-        wait -n
+        wait
     fi
 done
 
@@ -100,10 +116,10 @@ wait
 echo "Inference on all MOT16 sequences DONE"
 
 echo "Moving data from experiment folder to MOT16"
-mv ~/Yolov5_DeepSort_Pytorch/runs/track/exp/* \
-   ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/data/trackers/mot_challenge/MOT16-train/ch_yolov5m_deep_sort/data/
+mv runs/track/exp/* \
+   MOT16_eval/TrackEval/data/trackers/mot_challenge/MOT16-train/ch_yolov5m_deep_sort/data/
 
 # run the evaluation
-python ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/scripts/run_mot_challenge.py --BENCHMARK MOT16 \
+python MOT16_eval/TrackEval/scripts/run_mot_challenge.py --BENCHMARK MOT16 \
  --TRACKERS_TO_EVAL ch_yolov5m_deep_sort --SPLIT_TO_EVAL train --METRICS CLEAR Identity \
  --USE_PARALLEL False --NUM_PARALLEL_CORES 4
