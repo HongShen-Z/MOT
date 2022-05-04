71658f2a09c3e6ebdb9d7701435ab77f701685d0
ZHS <com.zhs@gmail.com>
2022-05-04 19:13:45 +0800

    update matching algo

--- track.py
+++ track.py
@@ -116,7 +116,7 @@ def detect(opt):
                 max_dist=cfg.DEEPSORT.MAX_DIST,
                 max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
                 max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
-                _lambda=0.3, _alpha=0.6,
+                _lambda=0.1, _alpha=0.8,
             )
         )
     outputs = [None] * nr_sources
