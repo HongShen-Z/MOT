2eedbbb04add47e08a9fdf8377665d08ca41cab1
ZHS <com.zhs@gmail.com>
2023-04-19 00:16:13 +0800

    deepsort-0.3

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#BSUB -J eval
+#BSUB -o %J.out
+#BSUB -e %J.err
+#BSUB -q gpu_v100
+#BSUB -gpu "num=1:mode=exclusive_process:aff=yes"
+
+module load anaconda3
+source activate
+conda deactivate
+conda activate torch1.10
+echo '1...'
